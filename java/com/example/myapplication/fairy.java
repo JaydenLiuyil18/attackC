573967c1e83b5f8b17b3ada751ce9dc491a53436
JaydenLiuyil18 <yil18@illinois.edu>
2020-04-23 08:22:23 -0500

    First commit

---
+++ java/com/example/myapplication/fairy.java
@@ -0,0 +1,64 @@
+package com.example.myapplication;
+
+public class fairy {
+    /**
+     * name
+     */
+    private String name;
+    /**
+     * level
+     */
+    private int level;
+    /**
+     * attack harm
+     */
+    private int attack;
+    /**
+     * attack interval
+     */
+    private double interval;
+    /**
+     * constructor
+     * @param n name.
+     * @param l level.
+     * @param a attack.
+     * @param i interval.
+     */
+    public fairy(String n, int l, int a, double i) {
+        name = n;
+        level = l;
+        attack = a;
+        interval = i;
+    }
+    /**
+     * get name.
+     * @return name.
+     * "" should be name; i need to change to commit
+     */
+    public String getName() {
+        return name;
+    }
+    /**
+     * get level
+     * @return level.
+     */
+    public int getLevel() {
+        return level;
+    }
+    /**
+     * get attack harm
+     * @return attack.
+     */
+    public int getHarm() {
+        return attack;
+    }
+
+    /**
+     * get interval.
+     * @return interval.
+     */
+    public double getInterval() {
+        return interval;
+    }
+
+}
