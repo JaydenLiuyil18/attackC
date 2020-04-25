5c1d91225de2800ac5fc733323daa6b218676627
JaydenLiuyil18 <yil18@illinois.edu>
2020-04-25 12:49:06 -0500

    first step of MP5 + second

--- java/com/example/myapplication/MainActivity.java
+++ java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+//import java.net.MalformedURLException;
+
+/**
+ * downloading url
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * oncreate;
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * dowanload URL
+     * @param src
+     * @return
+     */
+    public Bitmap getBitmapFromURL(String src) {
+        try {
+            java.net.URL url = new java.net.URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * download url incase of runoutof memory
+     * @param bm
+     * @param newHeight
+     * @param newWidth
+     * @return
+     */
+    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // CREATE A MATRIX FOR THE MANIPULATION
+        Matrix matrix = new Matrix();
+        // RESIZE THE BIT MAP
+        matrix.postScale(scaleWidth, scaleHeight);
+
+        // "RECREATE" THE NEW BITMAP
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height,
+                matrix, false);
+
+        return resizedBitmap;
     }
 }
--- java/com/example/myapplication/fairy.java
+++ java/com/example/myapplication/fairy.java
@@ -19,16 +19,20 @@ public class fairy {
     private double interval;
     /**
      * constructor
-     * @param n name.
-     * @param l level.
-     * @param a attack.
-     * @param i interval.
+     * @param a
+     *
      */
-    public fairy(String n, int l, int a, double i) {
-        name = n;
-        level = l;
-        attack = a;
-        interval = i;
+    private fairy f;
+    //其实我觉得如果我要计算英雄的伤害那每秒伤害肯定是a/i
+    //然后我在另一个class里call的话 fairy first = new fairy（first）
+    // 我觉得不一定要在construction里给定属性，属性应该是固定好的
+    // 先不设定升级，你看一下能不能按照我在attackcalculator的方式比如先固定5个英雄
+    public fairy(fairy a) {
+//        name = n;
+//        level = l;
+//        attack = a;
+//        interval = i;
+        f = a;
     }
     /**
      * get name.
